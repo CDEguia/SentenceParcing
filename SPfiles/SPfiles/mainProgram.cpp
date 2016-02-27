765c4422cc7089581f114f30db97834779c469df
Candelario Daniel Eguia <candelario.eguia@gmail.com>
2016-02-26 16:57:57 -0800

    First Commit

---
+++ SPfiles/SPfiles/mainProgram.cpp
@@ -0,0 +1,10 @@
+//------------------------------------------------------------------------------------------
+//          Name		Candelario D. Eguia
+//          Course		CMPS-455
+//          Project		No.2
+//          Due date	Feb. 26, 2015
+//          Professor	Ray Ahmadnia
+//
+// This program displays:
+//       Evaluate postfix expressions with multiple letter variables
+//------------------------------------------------------------------------------------------
